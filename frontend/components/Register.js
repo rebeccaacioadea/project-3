115d62c81d03f1e8b42843c934f7594202b9410c
bacxhus <enricxpieri@gmail.com>
2020-11-16 16:28:48 +0000

    UI Changes

--- frontend/components/Register.js
+++ frontend/components/Register.js
@@ -12,7 +12,7 @@ const Register = (props) => {
     password: '',
     passwordConfirmation: '',
     postcode: ''
-  
+
   })
   const [errors, updateErrors] = useState({
     name: '',
@@ -21,10 +21,10 @@ const Register = (props) => {
     password: '',
     passwordConfirmation: '',
     postcode: ''
- 
+
   })
 
-  
+
 
   function handleChange(event) {
     const name = event.target.name
@@ -54,151 +54,118 @@ const Register = (props) => {
           updateErrors(resp.data.error)
         } else {
           props.history.push('/user/login')
-        }   
+        }
       })
   }
   console.log(formData)
-  
-  return <form onSubmit={handleSubmit}>
-    <h1> Create Account</h1>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Full Name</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
-              placeholder=""
-              onChange = {handleChange}
-              value = {formData.name}
+  return <main>
+    <section className="cover">
+      <h1>Register</h1>
+    </section>
+
+    <section className="content" id="register">
+      <section className="margin">
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-section">
+            <label className="label"><h5>Name</h5></label>
+            <input className="input"
+              type="text"
+              placeholder="Type Here"
+              onChange={handleChange}
+              value={formData.name}
               name="name"
             />
             {errors.name && <p style={{ color: 'red' }}>
               {`There was a problem with your ${errors.name.path}`}
             </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Username</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
+          <div className="form-section">
+            <label className="label"><h5>Username</h5></label>
+            <input className="input"
+              type="text"
               placeholder=""
-              onChange = {handleChange}
-              value = {formData.userName}
+              onChange={handleChange}
+              value={formData.userName}
               name="userName"
             />
             {errors.userName && <p style={{ color: 'red' }}>
               {`There was a problem with your ${errors.userName.path}`}
             </p>}
+            {errors.name && <p style={{ color: 'red' }}>
+              {`There was a problem with your ${errors.name.path}`}
+            </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Email</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
+
+          <div className="form-section">
+            <label className="label"><h5>Email</h5></label>
+            <input className="input"
+              type="text"
               placeholder=""
-              onChange = {handleChange}
-              value = {formData.email}
+              onChange={handleChange}
+              value={formData.email}
               name="email"
             />
             {errors.email && <p style={{ color: 'red' }}>
               {`There was a problem with your ${errors.email.path}`}
             </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Password</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
+          <div className="form-section">
+            <label className="label"><h5>Password</h5></label>
+            <input className="input"
+              type="text"
               placeholder=""
-              onChange = {handleChange}
-              value = {formData.password}
+              onChange={handleChange}
+              value={formData.password}
               name="password"
             />
             {errors.password && <p style={{ color: 'red' }}>
               {`There was a problem with your ${errors.password.path}`}
             </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Confirm Password</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
+          <div className="form-section">
+            <label className="label"><h5>Confirm Password</h5></label>
+            <input className="input"
+              type="text"
               placeholder=""
-              onChange = {handleChange}
-              value = {formData.passwordConfirmation}
+              onChange={handleChange}
+              value={formData.passwordConfirmation}
               name="passwordConfirmation"
             />
             {errors.passwordConfirmation && <p style={{ color: 'red' }}>
               {'Does not match password'}
             </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">PostCode</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" 
-              type="text" 
+
+          <div className="form-section">
+            <label className="label"><h5>Postcode</h5></label>
+            <input className="input"
+              type="text"
               placeholder=""
-              onChange = {handleChange}
-              value = {formData.postcode}
+              onChange={handleChange}
+              value={formData.postcode}
               name="postcode"
             />
             {errors.postcode && <p style={{ color: 'red' }}>
               {`There was a problem with your ${errors.postcode.path}`}
             </p>}
           </div>
-        </div>
-      </div>
-    </div>
 
-    <div className="control">
-      <button className="button is-warning">Sign Up</button>
-    </div>
+          <button className="button-green" id="register-button">Sign Up</button>
 
-  </form>
+
+        </form>
+      </section>
+    </section>
+  </main>
 }
 
 export default Register
+
+
