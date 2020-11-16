239678d3b3dcf4641d819a37b96f2770b68343cc
bacxhus <enricxpieri@gmail.com>
2020-11-16 10:20:49 +0000

    home and UI

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { getUserId } from '../lib/auth'
-import Logo from '../images/BG.jpeg'
+import { Link } from 'react-router-dom'
 
 
 // ! This is the landing page
@@ -20,7 +20,6 @@ const Home = () => {
   const token = localStorage.getItem('token')
 
   if (!token) return <section>
-    <h1>Hi there</h1>
     <h2>No one logged in here!</h2>
   </section>
 
@@ -30,14 +29,83 @@ const Home = () => {
     })
       .then(resp => {
         updateUser(resp.data)
+        console.log(resp.data)
       })
   }, [])
 
-  return <main> 
-    <section className="header">
-      <h1>hello</h1>
+  return <main>
+    <section className="cover">
+      <h1>Welcome</h1>
     </section>
 
+    <section className="content">
+      <section className="margin">
+        <h2>{user.name}</h2>
+        <div className="header-title">
+          {/* Making left section and right edit icon align center */}
+          <div className="header-icon">
+            {/* Making 'Messages' and the icon align center */}
+            <img src="./images/messages.svg" alt="message-icon" />
+            <h6>Messages</h6>
+          </div>
+          <div>
+            <img src="./images/edit.svg" alt="edit-icon" />
+          </div>
+        </div>
+
+        <section className="status">
+          <div>
+            <img src="./images/badge.svg" alt="badge-icon" />
+            <h5>Support<br />Badge</h5>
+          </div>
+          <img src="./images/vertical-line.svg" alt="vertical-line" />
+          <div>
+            <img src="./images/plantpot.svg" alt="flower-in-pot" />
+            <h5>Looking<br />For</h5>
+          </div>
+          <img src="./images/vertical-line.svg" alt="vertical-line" />
+          <div>
+            <h1>0</h1>
+            <h5>Plant<br />Count</h5>
+          </div>
+        </section>
+
+        <hr />
+        <section className="list">
+          <Link to={`/user-page/${user._id}/`}>
+            <div className="list-item" id="home-profile">
+              <img src="./images/profile.svg" alt="profile" />
+              <h3>Profile</h3>
+            </div>
+          </Link>
+          <Link to={'/plant-search/'}>
+            <div className="list-item" id="home-search">
+              <img src="./images/magnifying.svg" alt="magnifying" />
+              <h3>Plant Search</h3>
+            </div>
+          </Link>
+          <Link to={'/user-map/'}>
+            <div className="list-item" id="home-map">
+              <img src="./images/map.svg" alt="map" />
+              <h3>User Map</h3>
+            </div>
+          </Link>
+          <Link to={'/fernstagram/'}>
+            <div className="list-item" id="home-ferngram">
+              <img src="./images/leaves.svg" alt="leaves" />
+              <h3>FernGram</h3>
+            </div>
+          </Link>
+          <Link to={'/plant-sitters/'}>
+            <div className="list-item" id="home-pinboard">
+              <img src="./images/pinboard.svg" alt="pinboard" />
+              <h3>Pin Board</h3>
+            </div>
+          </Link>
+        </section>
+
+      </section>
+    </section>
   </main>
 }
 
